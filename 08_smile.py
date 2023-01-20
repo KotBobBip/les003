9141e26f13e7320292114d992eb2046f23a30cf8
PavLove <gorilla33333@gmail.com>
2023-01-20 20:20:25 +0300

    Нарисовал наконец-то смайлик

--- 08_smile.py
+++ 08_smile.py
@@ -1,13 +1,39 @@
 # -*- coding: utf-8 -*-
 
 # (определение функций)
-import simple_draw
+import simple_draw as sd
 
 # Написать функцию отрисовки смайлика в произвольной точке экрана
 # Форма рожицы-смайлика на ваше усмотрение
 # Параметры функции: кордината X, координата Y, цвет.
 # Вывести 10 смайликов в произвольных точках экрана.
 
-# TODO здесь ваш код
+def draw_smile(coord_x, coord_y, color):
+    center_point = sd.get_point(coord_x, coord_y)
+    # Рисуем голову
+    center_point_head = center_point
+    sd.circle(center_point_head, 50, sd.COLOR_YELLOW, 1)
 
-simple_draw.pause()
+    # Рисуем улыбку в два подхода
+    center_point_smile1 = sd.get_point(center_point.x, center_point.y - 20)
+    sd.circle(center_point_smile1, 20, sd.COLOR_WHITE, 0)
+
+    center_point_smile2 = center_point
+    sd.circle(center_point_smile2, 30, sd.background_color, 0)
+
+    # Рисуем глаза
+    # Левый глаз
+    center_point_left_eye = sd.get_point(center_point.x - 20, center_point.y + 10)
+    sd.circle(center_point_left_eye, 10, sd.COLOR_BLACK, 0)
+
+    # Правый глаз
+    center_point_right_eye = sd.get_point(center_point.x + 20, center_point.y + 10)
+    sd.circle(center_point_right_eye, 10, sd.COLOR_BLACK, 0)
+
+
+draw_smile(100, 100, sd.COLOR_YELLOW)
+draw_smile(200, 200, sd.COLOR_YELLOW)
+draw_smile(300, 300, sd.COLOR_YELLOW)
+
+# sd.lines(point_list, sd.COLOR_YELLOW)
+sd.pause()
